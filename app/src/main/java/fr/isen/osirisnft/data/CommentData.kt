df41716d7f7498be70857db8bea4cc8e2a28ccf4
elise.rey <elise.rey@isen.yncrea.fr>
2022-03-16 11:34:21 +0100

    comment & reply classes

---
+++ app/src/main/java/fr/isen/osirisnft/data/CommentData.kt
@@ -0,0 +1,13 @@
+package fr.isen.osirisnft.data
+
+import com.google.gson.annotations.SerializedName
+import java.io.Serializable
+
+class CommentData(
+    @SerializedName("_id") val id: String,
+    @SerializedName("user") val user: String,
+    @SerializedName("publication_date") val date: String,
+    @SerializedName("content") val content: String,
+    @SerializedName("likes_count") val likes: Int,
+    @SerializedName("replies") val replies: List<ReplyData>
+): Serializable {}

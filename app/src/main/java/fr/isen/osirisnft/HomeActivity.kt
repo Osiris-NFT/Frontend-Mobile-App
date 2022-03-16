1ce4e992992dac45c609049ac3f668f480b16813
Yoann Renard <yoann.rnrd@gmail.com>
2022-03-16 17:06:01 +0100

    Parse publication ok

--- app/src/main/java/fr/isen/osirisnft/HomeActivity.kt
+++ app/src/main/java/fr/isen/osirisnft/HomeActivity.kt
@@ -8,8 +8,10 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import com.android.volley.Request
 import com.android.volley.toolbox.JsonObjectRequest
 import com.android.volley.toolbox.Volley
+import fr.isen.osirisnft.data.CommentData
 import fr.isen.osirisnft.data.Constants
 import fr.isen.osirisnft.data.PublicationData
+import fr.isen.osirisnft.data.ReplyData
 import fr.isen.osirisnft.databinding.ActivityHomeBinding
 import org.json.JSONArray
 import org.json.JSONException
@@ -24,9 +26,10 @@ class HomeActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         makeRequest()
+
     }
 
-    fun setPubList(publications: List<PublicationData>) { // envoie les données à l'adapter pour qu'il complète les champs
+    fun setPubList(publications: ArrayList<PublicationData>) { // envoie les données à l'adapter pour qu'il complète les champs
         binding.listOfPublication.layoutManager = LinearLayoutManager(this)
         binding.listOfPublication.adapter = PublicationAdapter(publications) { selectedPub ->
             showDetails(selectedPub)
@@ -49,7 +52,7 @@ class HomeActivity : AppCompatActivity() {
             parameters,
             {
                 Log.d("debug", it.toString(2))
-                parseResult(it)
+                parsePublications(it)
             },
             {
                 Log.d("debug", "$it")
@@ -58,22 +61,67 @@ class HomeActivity : AppCompatActivity() {
         queue.add(request)
     }
 
-    private fun parseResult(json: JSONObject) { // parse des données en une liste de publications
-        val array: JSONArray = json["publications"] as JSONArray
-        val arraylist = ArrayList<PublicationData>()
-        for (i in 0 until array.length()) {
-            var publication = array.get(i) as JSONObject
-            var comments = publication["comments"] as JSONArray
-            for (i in 0 until comments.length()){
-                Log.d("COMMENTS", comments.get(i).toString())
+    private fun parsePublications(json: JSONObject) { // parse des données en une liste de publications
+        val publications: JSONArray = json["publications"] as JSONArray
+        val arraylistofpub = ArrayList<PublicationData>()
+        for (i in 0 until publications.length()) {
+            Log.d("PUBLICATION", publications.get(i).toString())
+            val arraylistofcom = ArrayList<CommentData>()
+            val publication = publications.get(i) as JSONObject
+            val comments = publication["comments"] as JSONArray
+            for (y in 0 until comments.length()){
+                Log.d("COMMENT", comments.get(y).toString())
+                val arraylistofrep = ArrayList<ReplyData>()
+                val comment = comments.get(y) as JSONObject
+                val replies = comment["replies"] as JSONArray
+                for (z in 0 until replies.length()){
+                    val reply = replies.get(z) as JSONObject
+                    //Log.d("REPLY", reply.toString())
+                    val replydata =  ReplyData(
+                        reply["_id"] as String,
+                        reply["user"] as String,
+                        reply["publication_date"] as String,
+                        reply["content"] as String,
+                        reply["likes_count"] as Int)
+                    Log.d("REPLY AS REPLYDATA", replydata.toString())
+                    arraylistofrep.add(replydata)
+                }
+                val commentdata = CommentData(
+                    comment["_id"] as String,
+                    comment["user"] as String,
+                    comment["publication_date"] as String,
+                    comment["content"] as String,
+                    comment["likes_count"] as Int,
+                    arraylistofrep,
+                )
+                arraylistofcom.add(commentdata)
             }
-            Log.d("PUBLICATIONS", publication.toString())
+            val arraylistofhash = ArrayList<String>()
+            val hashtags = publication["hashtags"] as JSONArray
+            for (w in 0 until hashtags.length()){
+                val hashtag = hashtags.get(w) as String
+                arraylistofhash.add(hashtag)
+            }
+            val publicationdata = PublicationData(
+                publication["_id"] as String,
+                publication["publication_date"] as String,
+                publication["publication_name"] as String,
+                publication["user_name"] as String,
+                publication["content_type"] as String,
+                publication["media_url"] as String,
+                publication["category"] as String,
+                publication["description"] as String,
+                arraylistofhash,
+                publication["likes_count"] as Int,
+                arraylistofcom
+            )
+            arraylistofpub.add(publicationdata)
             //Log.d("REQUEST", array.get(i).toString())
 
             //arraylist.add(array.get(i) as PublicationData)
         }
-        Log.d("REQUEST", arraylist.toString())
-
+        Log.d("PUBLICATIONS AS ARRAYLIST", arraylistofpub.toString())
+        setPubList(arraylistofpub)
     }
 
     companion object {
